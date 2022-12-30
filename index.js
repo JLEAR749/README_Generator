1476b0ddb03429b5e698dfe5fd22c77151c56b7d
J0J0 <iditaroddream@gmail.com>
2022-12-29 19:49:31 -0600

    added generatemarkdown and question

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) { }
+// fs.writeFile('index.html', htmlPageContent, (err) =>
+// err ? console.log(err) : console.log('Successfully created index.html!')
+// );
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions).then((response) => { 
+            console.log(response)
+            console.log(generateMarkdown(response))
+         }).catch((err) => { console.log(err) })
+}
+
 
 // Function call to initialize app
 init();
